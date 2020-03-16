13cdcf05b73ead4df6138a9fdefd52ba42ae7e73
uranussg <uranussg@gmail.com>
2020-03-16 16:45:01 -0700

    delete tweets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
@@ -29,5 +29,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
 //   ReactDOM.render(<Root store={store} />, root);
-  ReactDOM.render(<h1>React is working</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
