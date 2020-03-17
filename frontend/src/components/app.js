aeec2ba9224c22df463cd3e87ab70b319ea5bfb4
uranussg <uranussg@gmail.com>
2020-03-16 17:07:44 -0700

    debugged

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,8 +6,8 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
+import LoginFormContainer from './sesssion/login_form_container';
+import SignupFormContainer from './sesssion/signup_form_container';
 
 const App = () => (
   <div>
