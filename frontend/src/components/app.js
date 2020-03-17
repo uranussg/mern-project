045418ff4f479c4d0105f07ca74e5df4e8517e80
Jon Odom <odomj5@gmail.com>
2020-03-17 11:56:30 -0700

    Updated login form container for modal

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,3 @@
-// src/components/app.js
-
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
@@ -11,7 +9,7 @@ import SignupFormContainer from './sesssion/signup_form_container';
 
 const App = () => (
   <div>
-    
+
     <NavBarContainer />
     <Switch>
         <AuthRoute exact path="/" component={MainPage} />
