aefe969e7864b3c2e3e5734cead60bdfa17b92b8
rakin <rakinadm19@gmail.com>
2020-03-18 10:52:14 -0700

    Added background styling to main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import "./main_page.css";
+import backgroundImage from "../../images/gameroom3.jpg";
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="main-page-container">
+        {/* <div className="main-page-background"></div> */}
+        <img className="main-page-image" src={backgroundImage} />
         <p>The Game Room</p>
         <footer>
           Copyright &copy; 2020 the Game Room
         </footer>
+      <div className="testcontent"></div>
       </div>
     );
   }
