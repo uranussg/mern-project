89d58c1dd2e5a9075bb1e945e50d849b6657c3bc
uranussg <uranussg@gmail.com>
2020-04-03 14:42:30 -0700

    remove logger in production

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
-// const composedEnhancer = compose(applyMiddleware(thunk, logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composedEnhancer = compose(applyMiddleware(thunk, logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const enhanser =  {
+  development: composedEnhancer
+  ,
+  production: applyMiddleware(thunk)
+  
+}
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    enhanser[process.env.NODE_ENV]
   )
 );
 export default configureStore;
+
