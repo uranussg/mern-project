5ad11821e94344577162214ffcfca3f64d2bab89
Jon Odom <odomj5@gmail.com>
2020-04-15 12:04:47 -0700

    Styled tutorial

--- frontend/src/components/main/tutorial.jsx
+++ frontend/src/components/main/tutorial.jsx
@@ -9,17 +9,20 @@ class Tutorial extends React.Component {
 
     return (
       <div className="tutorial-div">
+          <h1>Theme Room Tutorial</h1>
         <div className="tutorial-headline">
-          <h1>Theme Room Turotial</h1>
           <p>The Theme Room is a chat app that allows users to pick a room theme</p>
         </div>
         <div className="tutorial-steps">
-          <h2>Singup or Login to access chat rooms</h2>
-          <h2>One logged in, join an existing game or start a new room to become the game admin</h2>
-          <h2>The admin can activate a role-playing theme </h2>
+          <h2>Instructions</h2>
+          <ol>
+            <li>Singup or Login to access chat rooms</li>
+            <li>Join an existing game or start a new room to become the game admin</li>
+            <li>The admin can activate a role-playing theme</li>
+          </ol>
         </div>
         <div className="themes-cont">
-          <h2>Choose Game Room Theme</h2>
+          <h2>Choose Room Theme</h2>
           <div className="choose-theme-cont">
             <div>
               <img src={GOT} alt="Game of Thrones"/>
